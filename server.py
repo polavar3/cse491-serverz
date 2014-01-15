e775be08877179605ce36d0269ffa91b9c4d1058
Taylor Jones <jonest31@arctic.cse.msu.edu>
2014-01-14 19:23:05 -0500

    mychanges

--- server.py
+++ server.py
@@ -17,7 +17,10 @@ print 'Entering infinite loop; hit CTRL-C to exit'
 while True:
     # Establish connection with client.    
     c, (client_host, client_port) = s.accept()
+
     print 'Got connection from', client_host, client_port
-    c.send('Thank you for connecting')
-    c.send("good bye.")
+    c.send("HTTP/1.0 200 OK\r\n" +
+           "Content-Type: text/html\r\n\r\n"+
+           "<h1>Hello, world</h1> This is jonest31's Web server!")
+    
     c.close()
